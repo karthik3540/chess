93c2bd0623ed2f0491ea2171b1f8f2774ae969a7
karthik3540 <karthikratnala45@gmail.com>
2025-05-31 22:03:18 +0530

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA8SiCKZnRQXWnrR0wPbPEDE2ir8iRcWYo",
+  authDomain: "chess-51f68.firebaseapp.com",
+  projectId: "chess-51f68",
+  storageBucket: "chess-51f68.firebasestorage.app",
+  messagingSenderId: "958322982734",
+  appId: "1:958322982734:web:ca6d9d814bc4d20efde81f",
+  measurementId: "G-F3XJFG127N"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
